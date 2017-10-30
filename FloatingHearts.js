b71ffb116e202e6a5370f30f896c60006accc2df
Adam Denny <adam.m.denny2@gmail.com>
2017-10-30 12:25:29 -0700

    react native 0.47 fix

--- FloatingHearts.js
+++ FloatingHearts.js
@@ -1,5 +1,6 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 import { View, Animated, StyleSheet } from 'react-native'
+import { PropTypes } from 'prop-types';
 
 import HeartShape from './HeartShape'
 
